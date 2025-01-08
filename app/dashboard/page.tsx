ce030b253f9a5f38ecb735b74626c4b0e639f779
tommy-vpr <tommy@cultivatedagency.com>
2025-01-08 14:47:31 -0800

    update dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
+import { Check, SquarePen } from "lucide-react";
+import Link from "next/link";
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
@@ -8,9 +10,67 @@ const Dashboard = async () => {
 
   return (
     <div>
-      <h3 className="text-3xl font-semibold capitalize">
-        {firstName || session?.user.name}'s Dashboard
+      <h3 className="text-3xl font-semibold uppercase mb-8">
+        Welcome, {firstName || session?.user.name}!
       </h3>
+      <div className="flex gap-8">
+        <div className="w-1/2 rounded-lg overflow-hidden">
+          <video autoPlay muted loop className="w-full h-auto">
+            <source
+              src="https://cdn.shopify.com/videos/c/o/v/d8a05164335a4c9d8ec5a5ac6bd5fe0b.mp4"
+              type="video/mp4"
+            />
+          </video>
+        </div>
+        <div>
+          <h3 className="font-semibold uppercase text-3xl mb-2 text-green-400">
+            Budtender Perks
+          </h3>
+          <ul className="space-y-2 text-sm mb-4">
+            <li className="flex gap-1">
+              <Check size={14} />
+              Exclusive Discounts or Promotions
+            </li>
+            <li className="flex gap-1">
+              <Check size={14} />
+              Access to Exclusive Content
+            </li>
+            <li className="flex gap-1">
+              <Check size={14} />
+              Early Access
+            </li>
+            <li className="flex gap-1">
+              <Check size={14} />
+              Community Perks
+            </li>
+          </ul>
+          <Link
+            href="https://itslitto.com/"
+            className="bg-green-400 text-[#333] font-semibold text-sm py-2 px-4 rounded-sm hover:bg-green-500"
+          >
+            Check out our website
+          </Link>
+        </div>
+      </div>
+      <div className="mt-12">
+        <h3 className="font-semibold uppercase text-lg mb-2">
+          Feeling Confident? Take a Test
+        </h3>
+        <div className="flex gap-4">
+          <div className="bg-[#333] p-4 rounded-sm flex items-center gap-2">
+            <SquarePen size={14} />
+            Hemp Test
+          </div>
+          <div className="bg-[#333] p-4 rounded-sm flex items-center gap-2">
+            <SquarePen size={14} />
+            Cannabis Test
+          </div>
+          <div className="bg-[#333] p-4 rounded-sm flex items-center gap-2">
+            <SquarePen size={14} />
+            Strains Test
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ data.ts
@@ -0,0 +1,234 @@
+export const data = {
+  original: [
+    {
+      title: "Maui Wowie",
+      imageUrl: "/images/LITTO-V2-1G-MAIN-Maui_Wowie.webp",
+      strain: "Sativa",
+      price: "$60",
+      url: "https://itslitto.com/pages/cannabis/maui-wowie",
+    },
+    {
+      title: "Blue Dream",
+      imageUrl: "/images/LITTO-V2-1G-MAIN-Blue_Dream.webp",
+      strain: "Sativa",
+      price: "$60",
+      url: "https://itslitto.com/pages/cannabis/blue-dream",
+    },
+    {
+      title: "Pineapple Express",
+      imageUrl: "/images/LITTO-V2-1G-MAIN-Pineapple_Express.webp",
+      strain: "Sativa",
+      price: "$60",
+      url: "https://itslitto.com/pages/cannabis/pineapple-express-1",
+    },
+    {
+      title: "Blueberry Kush",
+      imageUrl: "/images/LITTO-V2-1G-MAIN-Blueberry_Kush.webp",
+      strain: "Sativa",
+      price: "$60",
+      url: "https://itslitto.com/pages/cannabis/blueberry-kush",
+    },
+    // {
+    //   title: "Grape God",
+    //   imageUrl: "/images/LITTO-V2-1G-MAIN-Grape_God.webp",
+    //   strain: "Sativa",
+    //   price: "$60",
+    // },
+  ],
+  exotic: [
+    {
+      title: "Gushers",
+      imageUrl: "/images/LITTO-V2-1G-MAIN-Gushers.webp",
+      url: "https://itslitto.com/pages/cannabis/gushers",
+    },
+    {
+      title: "Apple Fritter",
+      imageUrl: "/images/LITTO-V2-1G-MAIN-Apple_Fritter.webp",
+      url: "https://itslitto.com/pages/cannabis/apple-fritter",
+    },
+    {
+      title: "Betty White",
+      imageUrl: "/images/LITTO-V2-1G-MAIN-Betty_White.webp",
+      url: "https://itslitto.com/pages/cannabis/betty-white",
+    },
+    {
+      title: "Runtz",
+      imageUrl: "/images/LITTO-V2-1G-MAIN-Runtz.webp",
+      url: "https://itslitto.com/pages/cannabis/runtz",
+    },
+  ],
+  liveResin: [
+    {
+      title: "Dragon",
+      imageUrl: "/images/Dragon_0006.webp",
+      url: "https://itslitto.com/pages/cannabis/dragon",
+    },
+    {
+      title: "Jack Herer",
+      imageUrl: "/images/JackHerer_0006.webp",
+      url: "https://itslitto.com/pages/cannabis/jack-herer",
+    },
+    {
+      title: "Shmac-1",
+      imageUrl: "/images/LITTO-V2-LR-1G-MAIN-Shmac_1.webp",
+      url: "https://itslitto.com/pages/cannabis/shmac-1",
+    },
+    {
+      title: "White Runtz",
+      imageUrl: "/images/LITTO-V2-LR-1G-MAIN-White_Runtz.webp",
+      url: "https://itslitto.com/pages/cannabis/white-runtz",
+    },
+    // {
+    //   title: "Tropical Zkittles",
+    //   imageUrl: "/images/TropicalZkittles.webp",
+    // },
+  ],
+  edibles: [
+    {
+      title: "Baja Blast",
+      imageUrl: "/images/litto-bites-100mg-x-10mg-baja_blast.webp",
+      url: "https://itslitto.com/pages/cannabis/baja-blast",
+    },
+    {
+      title: "Blodd Orange",
+      imageUrl: "/images/litto-bites-100mg-x-10mg-blood_orange.webp",
+      url: "https://itslitto.com/pages/cannabis/blood-orange",
+    },
+    {
+      title: "Green Apple Yuzu",
+      imageUrl: "/images/litto-bites-100mg-x-10mg-green_apple_yuzu.webp",
+      url: "https://itslitto.com/pages/cannabis/green-apple-yuzu",
+    },
+    {
+      title: "Pink Lemonade",
+      imageUrl: "/images/litto-bites-100mg-x-10mg-pink_lemonade.webp",
+      url: "https://itslitto.com/pages/cannabis/pink-lemonade",
+    },
+    // {
+    //   title: "Strawberry Banana",
+    //   imageUrl: "/images/litto-bites-100mg-x-10mg-strawberry_banana.webp",
+    // },
+  ],
+};
+
+export const hemp = {
+  zooted: [
+    {
+      title: "Skunky OG",
+      imageUrl: "/images/ZOOTED-HYB-skunky_og.webp",
+      url: "https://itslitto.com/products/all-in-one-device-zooted-hybrid-thc-p-skunky-og-2g",
+    },
+    {
+      title: "White Widow",
+      imageUrl: "/images/ZOOTED-HYB-white_widow.webp",
+      url: "https://itslitto.com/products/all-in-one-device-zooted-sativa-thc-p-sour-poison-2g",
+    },
+    {
+      title: "Northern Lights",
+      imageUrl: "/images/ZOOTED-IND-northern_lights.webp",
+      url: "https://itslitto.com/products/all-in-one-device-zooted-indica-thc-p-northern-lights-2g",
+    },
+    {
+      title: "Purple Grape Daddy",
+      imageUrl: "/images/ZOOTED-IND-purple_grape_daddy.webp",
+      url: "https://itslitto.com/products/all-in-one-device-zooted-hybrid-thc-p-white-widow-2g",
+    },
+    // {
+    //   title: "Grapefruit",
+    //   imageUrl: "/images/ZOOTED-SAT-grapefruit.webp",
+    //   url: "",
+    // },
+    // {
+    //   title: "ZOOTED-SAT-sour_poison",
+    //   imageUrl: "/images/ZOOTED-SAT-sour_poison.webp",
+    // },
+  ],
+  smacked: [
+    {
+      title: "Durban Poison",
+      imageUrl: "/images/DurbanPoison.webp",
+      url: "https://itslitto.com/products/disposable-smckd-thcaliveresin-durbanpoison-2g",
+    },
+    {
+      title: "Legend OG",
+      imageUrl: "/images/LegendOG.webp",
+      url: "https://itslitto.com/products/disposable-smckd-thcaliveresin-legendog-2g",
+    },
+    {
+      title: "Mac 1",
+      imageUrl: "/images/Mac1.webp",
+      url: "https://itslitto.com/products/disposable-smckd-thcaliveresin-mac1-2g",
+    },
+    {
+      title: "Papaya Fiyah",
+      imageUrl: "/images/PapayaFiyah.webp",
+      url: "https://itslitto.com/products/disposable-smckd-thcaliveresin-papayafiyah-2g",
+    },
+  ],
+  ktfo: [
+    {
+      title: "Cherry Sherbet",
+      imageUrl: "/images/LITTO-KTFO-MAIN-Cherry_Sherbet.webp",
+      url: "https://itslitto.com/products/all-in-one-device-ktfo-hybrid-thca-cherry-sherbet-2g",
+    },
+    {
+      title: "Jack Herer",
+      imageUrl: "/images/LITTO-KTFO-MAIN-Jack_Herer.webp",
+      url: "https://itslitto.com/products/disposable-ktfo-thca-jackherer-2g",
+    },
+    {
+      title: "Mango OG",
+      imageUrl: "/images/LITTO-KTFO-MAIN-Mango_OG.webp",
+      url: "https://itslitto.com/products/disposable-ktfo-thca-mangoog-2g",
+    },
+    {
+      title: "Melon Crush",
+      imageUrl: "/images/LITTO-KTFO-MAIN-Melon_Crush.webp",
+      url: "https://itslitto.com/products/disposable-ktfo-thca-meloncrush-2g",
+    },
+  ],
+  faded: [
+    {
+      title: "Jack Herer",
+      imageUrl: "/images/LITTO-FADED-MAIN-Jack_Herer_large.webp",
+      url: "https://itslitto.com/products/disposable-faded-liveresin-jackherer-2g",
+    },
+    {
+      title: "Mango Slush",
+      imageUrl: "/images/LITTO-FADED-MAIN-Mango_Slush_large.webp",
+      url: "https://itslitto.com/products/disposable-faded-liveresin-mangoslush-2g",
+    },
+    {
+      title: "Strawbery Diesel",
+      imageUrl: "/images/LITTO-FADED-MAIN-Strawberry_Diesel_large.webp",
+      url: "https://itslitto.com/products/disposable-faded-liveresin-strawberrydiesel-2g",
+    },
+    {
+      title: "Watermelon OG",
+      imageUrl: "/images/LITTO-FADED-MAIN-Watermelon_OG_large.webp",
+      url: "https://itslitto.com/products/disposable-faded-liveresin-watermelonog-2g",
+    },
+  ],
+  preRolls: [
+    {
+      title: "Bubble Gum Glue",
+      imageUrl: "/images/lh-3g-dpr-bubble-gum-glue.webp",
+      url: "https://itslitto.com/products/hemp-pre-rolls-indica-bubble-gum-glue-thca",
+    },
+    {
+      title: "Cotton Candy",
+      imageUrl: "/images/lh-3g-dpr-cotton-candy.webp",
+      url: "https://itslitto.com/products/hemp-pre-rolls-hybrid-cotton-candy-thca",
+    },
+    {
+      title: "Sour Diesel",
+      imageUrl: "/images/lh-3g-dpr-sour-diesel.webp",
+      url: "https://itslitto.com/products/hemp-pre-rolls-sativa-sour-diesel-thca",
+    },
+    {
+      title: "Watermelon OG",
+      imageUrl: "/images/lh-3g-dpr-watermelon-og.webp",
+      url: "https://itslitto.com/products/hemp-pre-rolls-indica-watermelon-og-thca",
+    },
+  ],
+};

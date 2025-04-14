9d76a8e1ad8c631e245b3630fa94b13aa1192f56
tommy-vpr <tommy@cultivatedagency.com>
2025-04-14 13:42:24 -0700

    ./lib/generateCoupon.ts:1:10

--- lib/generateCoupon.ts
+++ lib/generateCoupon.ts
@@ -1,18 +1,18 @@
-import { generateDiscountCode } from "@/app/actions/admin";
-import { getErrorMessage } from "./getErrorMessage";
+// import { generateDiscountCode } from "@/app/actions/admin";
+// import { getErrorMessage } from "./getErrorMessage";
 
-export const generatedCoupon = async () => {
-  try {
-    const response = await generateDiscountCode();
+// export const generatedCoupon = async () => {
+//   try {
+//     const response = await generateDiscountCode();
 
-    if (response.success) {
-      return response.code || null; // Explicitly handle `undefined`
-    } else {
-      return {
-        error: "There was an error generating coupon",
-      };
-    }
-  } catch (err) {
-    return getErrorMessage(err);
-  }
-};
+//     if (response.success) {
+//       return response.code || null; // Explicitly handle `undefined`
+//     } else {
+//       return {
+//         error: "There was an error generating coupon",
+//       };
+//     }
+//   } catch (err) {
+//     return getErrorMessage(err);
+//   }
+// };
